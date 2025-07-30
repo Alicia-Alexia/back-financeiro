d55125489f23831f59ac0c237ea4275772fd55ab
Alicia <alicia.alexia03@gmail.com>
2025-07-30 08:24:31 -0300

    feat(model):cria model de despesa, empenho e pagamento

--- src/main/java/com/sop/Financeiro/model/Empenho.java
+++ src/main/java/com/sop/Financeiro/model/Empenho.java
@@ -1,4 +1,38 @@
 package com.sop.Financeiro.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "empenho")
 public class Empenho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String numeroEmpenho;
+
+    private LocalDate dataEmpenho;
+    private BigDecimal valorEmpenho;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "despesa_id")
+    private Despesa despesa;
+
+    @OneToMany(mappedBy = "empenho", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Pagamento> pagamentos;
 }
--- src/main/java/com/sop/Financeiro/model/Pagamento.java
+++ src/main/java/com/sop/Financeiro/model/Pagamento.java
@@ -1,4 +1,34 @@
 package com.sop.Financeiro.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pagamento")
 public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String numeroPagamento;
+
+    private LocalDate dataPagamento;
+    private BigDecimal valorPagamento;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "empenho_id")
+    private Empenho empenho;
 }
